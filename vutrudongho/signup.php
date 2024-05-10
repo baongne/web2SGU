bed4c05f075cf7616eb39f131cc84d92b882daeb
Quốc Bảo <113650395+baongne@users.noreply.github.com>
2024-05-10 17:56:55 +0700

    Update signup.php

--- vutrudongho/signup.php
+++ vutrudongho/signup.php
@@ -10,16 +10,12 @@
   <link rel="stylesheet" href="assets/css/account_registration_form.css">
   <link rel="stylesheet" href="assets/css/header.css">
   <link rel="stylesheet" href="assets/css/footer.css">
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap&amp;_cacheOverride=1679484892371"
-    data-tag="font">
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
-    data-tag="font">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap&amp;_cacheOverride=1679484892371" data-tag="font">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" data-tag="font">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.all.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.min.css" rel="stylesheet">
-    <script src="sweetalert2.min.js"></script>
-    <link rel="stylesheet" href="sweetalert2.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.min.css" rel="stylesheet">
+  <script src="sweetalert2.min.js"></script>
+  <link rel="stylesheet" href="sweetalert2.min.css">
 </head>
 
 <body>
@@ -33,19 +29,18 @@
   <!--End: Header-->
   <div id="body" style=" height: fit-content;margin-top: 50px;">
     <div class="containerlogin-containerlogin">
-    <div style="width: 40%;height: 100%;display:flex;flex-direction:column;align-items:center;justify-content:center;">
-      <img src="assets/img/hoangImg/logo/logo_tron.png" alt="z416347160358228d6ce2e5edbcf0ee0b207d1a4329bed23772"
-        class="containerlogin-z416347160358228d6ce2e5edbcf0ee0b207d1a4329bed2" onclick="">
-      <span class="containerlogin-text24 TitleMedium">
-        <span>VŨ TRỤ ĐỒNG HỒ</span>
-      </span>
-      <span class="containerlogin-text26">
-        <span>
-          <span>Nền tảng thương mại điện tử được yêu thích tại</span>
-          <br>
-          <span>Thành phố Hồ Chí Minh</span>
+      <div style="width: 40%;height: 100%;display:flex;flex-direction:column;align-items:center;justify-content:center;">
+        <img src="assets/img/hoangImg/logo/logo_tron.png" alt="z416347160358228d6ce2e5edbcf0ee0b207d1a4329bed23772" class="containerlogin-z416347160358228d6ce2e5edbcf0ee0b207d1a4329bed2" onclick="">
+        <span class="containerlogin-text24 TitleMedium">
+          <span>VŨ TRỤ ĐỒNG HỒ</span>
         </span>
-      </span>
+        <span class="containerlogin-text26">
+          <span>
+            <span>Nền tảng thương mại điện tử được yêu thích tại</span>
+            <br>
+            <span>Thành phố Hồ Chí Minh</span>
+          </span>
+        </span>
       </div>
       <form style="width: 60%;height: 100%;display:flex;flex-direction:column;align-items:center;justify-content:center;" name="frmdangky" id="" action=".//modules/signup_processing.php" method="POST" onsubmit="return kiemTra();">
 
@@ -63,58 +58,43 @@
           </div>
           <div>
             <p style="margin-top: 30px;margin-bottom: 4px;">Họ và tên (*)</p>
-            <input name="fullName" type="text"
-              style="padding-left: 6px;width:320px; height:36px;  border-style: outset;margin-bottom: 12px; border: 1px solid #674FA3; border-radius: 8px;margin-right: 16px;"
-              placeholder="Họ và tên">
+            <input name="fullName" type="text" style="padding-left: 6px;width:320px; height:36px;  border-style: outset;margin-bottom: 12px; border: 1px solid #674FA3; border-radius: 8px;margin-right: 16px;" placeholder="Họ và tên">
           </div>
           <div>
             <div style="display: flex; flex-direction:row ;width: fit-content;">
               <p style="margin-bottom: 4px;">Email </p>
-              <p id="error-message"
-                style="display: none; color: red;padding-left: 10px;font-size: 12px;font-weight: bold;line-height: 18.391px;">
+              <p id="error-message" style="display: none; color: red;padding-left: 10px;font-size: 12px;font-weight: bold;line-height: 18.391px;">
               </p>
             </div>
-            <input id="email" name="email" type="email"
-              style="padding-left: 6px;width:320px; height:36px; border-style: outset;margin-bottom: 12px; border: 1px solid #674FA3; border-radius: 8px;"
-              placeholder="Email">
+            <input id="email" name="email" type="email" style="padding-left: 6px;width:320px; height:36px; border-style: outset;margin-bottom: 12px; border: 1px solid #674FA3; border-radius: 8px;" placeholder="Email">
           </div>
           <div>
             <div style="display: flex; flex-direction:row ;width: fit-content;">
               <p style="margin-bottom: 4px;">Số điện thoại (*)</p>
-              <p id="phoneNumber-message"
-                style="display: none; color: red;padding-left: 10px;font-size: 12px;font-weight: bold;line-height: 18.391px;">
+              <p id="phoneNumber-message" style="display: none; color: red;padding-left: 10px;font-size: 12px;font-weight: bold;line-height: 18.391px;">
               </p>
             </div>
-            <input id="numberPhone" name="numberPhone" type="text"
-              style="padding-left: 6px;width:320px; height:36px; border-style: outset;margin-bottom: 12px; border: 1px solid #674FA3; border-radius: 8px;"
-              placeholder="Số điện thoại">
+            <input id="numberPhone" name="numberPhone" type="text" style="padding-left: 6px;width:320px; height:36px; border-style: outset;margin-bottom: 12px; border: 1px solid #674FA3; border-radius: 8px;" placeholder="Số điện thoại">
           </div>
           <div>
             <div style="display: flex; flex-direction:row ;width: fit-content;">
               <p style="margin-bottom: 4px;">Mật khẩu (*)</p>
-              <p id="pass-message"
-                style="display: none; color: red;padding-left: 10px;font-size: 12px;font-weight: bold;line-height: 18.391px;">
+              <p id="pass-message" style="display: none; color: red;padding-left: 10px;font-size: 12px;font-weight: bold;line-height: 18.391px;">
                 aa</p>
             </div>
 
-            <input id="password" name="passWord" type="password"
-              style="padding-left: 6px;width:320px; height:36px; border-style: outset;margin-bottom: 12px; border: 1px solid #674FA3; border-radius: 8px;"
-              placeholder="Mật khẩu" maxlength="20">
+            <input id="password" name="passWord" type="password" style="padding-left: 6px;width:320px; height:36px; border-style: outset;margin-bottom: 12px; border: 1px solid #674FA3; border-radius: 8px;" placeholder="Mật khẩu" maxlength="20">
           </div>
           <div>
             <div style="display: flex; flex-direction:row ;width: fit-content;">
               <p style="margin-bottom: 4px;">Nhập lại mật khẩu (*)</p>
-              <img id="khongtrungkhop" src="assets/img/hoangImg/icons/icons8-cancel-48.png" alt="" width="20px"
-                height="20px" style="margin-left: 10px;display: none;">
-              <img id="trungkhop" src="assets/img/hoangImg/icons/icons8-ok.gif" alt="" width="20px" height="20px"
-                style="margin-left: -20px;display: none;">
+              <img id="khongtrungkhop" src="assets/img/hoangImg/icons/icons8-cancel-48.png" alt="" width="20px" height="20px" style="margin-left: 10px;display: none;">
+              <img id="trungkhop" src="assets/img/hoangImg/icons/icons8-ok.gif" alt="" width="20px" height="20px" style="margin-left: -20px;display: none;">
             </div>
-            <input id="repeatPassword" name="repeatPassword" type="password"
-              style="padding-left: 6px;width:320px; height:36px; border-style: outset; border: 1px solid #674FA3; border-radius: 8px;margin-bottom: 20px;"
-              placeholder="Nhập lại mật khẩu" maxlength="20">
+            <input id="repeatPassword" name="repeatPassword" type="password" style="padding-left: 6px;width:320px; height:36px; border-style: outset; border: 1px solid #674FA3; border-radius: 8px;margin-bottom: 20px;" placeholder="Nhập lại mật khẩu" maxlength="20">
           </div>
 
-          <div>
+          <!-- <div>
             <p style="margin-top: 30px;margin-bottom: 4px;">Tỉnh/Thành phố (*)</p>
             <select id="city" name="tinh"
               style="width:332px; height:36px; border-style: outset;margin-bottom: 12px; border: 1px solid #674FA3; border-radius: 8px;">
@@ -134,29 +114,28 @@
               style="width:332px; height:36px; border-style: outset;margin-bottom: 12px; border: 1px solid #674FA3; border-radius: 8px;">
               <option value="" selected></option>
             </select>
-          </div>
+          </div> -->
           <div>
-            <p style="margin-bottom: 4px;">Địa chỉ nhận hàng (*)</p>
-            <input name="diaChiNha" type="text"
-              style="padding-left: 6px;width:332px; height:36px; border-style: outset;margin-bottom: 34px; border: 1px solid #674FA3; border-radius: 8px;"
-              placeholder="Số nhà, đường">
+            <p style="margin-top: 30px;">Địa chỉ nhận hàng (*)</p>
+            <input name="diaChiNha" type="text" style="padding-left: 6px;width:332px; height:36px; border-style: outset;margin-bottom: 34px; border: 1px solid #674FA3; border-radius: 8px;" placeholder="Số nhà, đường">
           </div>
-          <div style="width: 332px; display:flex;justify-content: center;">
-            <input type="submit" id="btnSubmitLogin" class="containerlogin-text06 LabelLarge" value="Đăng ký">
-            </span>
+          <div style="width: 500px; display: flex; justify-content: center; margin-top: 20px;">
+            <!-- Đoạn mã button với chỉnh sửa trực tiếp -->
+            <input type="submit" id="btnSubmitLogin" class="containerlogin-text06 LabelLarge" value="Đăng ký" style="width: 200px; height: 60px; font-size: 24px;">
           </div>
+
 
         </div>
 
       </form>
-      
+
 
     </div>
   </div>
 
   <!--Start: Footer-->
   <div id="my-footer" style="">
-  <?php
+    <?php
     include("components/footer.php");
     ?>
   </div>
@@ -170,7 +149,7 @@
     var errorMessage = document.getElementById("error-message");
 
     // Gắn sự kiện input cho phần tử input
-    inputMail.addEventListener("input", function () {
+    inputMail.addEventListener("input", function() {
       // Kiểm tra định dạng email
       var email = inputMail.value;
       var re = /\S+@\S+\.\S+/;
@@ -179,8 +158,7 @@
       // Nếu email không hợp lệ, hiển thị thông báo lỗi
       if (document.frmdangky.email.value.length == 0) {
         errorMessage.style.display = "none";
-      }
-      else {
+      } else {
         if (!valid) {
           errorMessage.innerText = "Định dạng email chưa đúng";
           errorMessage.style.display = "block";
@@ -212,24 +190,23 @@
     }
 
     // Gắn sự kiện input cho phần tử input
-    numberPhone.addEventListener("input", function () {
+    numberPhone.addEventListener("input", function() {
       var phoneNumber = numberPhone.value;
-      
+
       // Hiển thị thông báo tương ứng
-      if(phoneNumber.trim().length == 0){
+      if (phoneNumber.trim().length == 0) {
         numberPhoneMessage.innerText = ' ';
         numberPhoneMessage.style.display = "block";
+      } else {
+        if (isValidPhoneNumber(phoneNumber) == true) {
+          numberPhoneMessage.innerText = "Số điện thoại hợp lệ";
+          numberPhoneMessage.style.display = "block";
+          numberPhoneMessage.style.color = "#00CC00";
+        } else {
+          numberPhoneMessage.innerText = "Số điện thoại không hợp lệ";
+          numberPhoneMessage.style.display = "block";
+        }
       }
-      else{
-      if (isValidPhoneNumber(phoneNumber) == true) {
-        numberPhoneMessage.innerText = "Số điện thoại hợp lệ";
-        numberPhoneMessage.style.display = "block";
-        numberPhoneMessage.style.color = "#00CC00";
-      } 
-      else {
-        numberPhoneMessage.innerText = "Số điện thoại không hợp lệ";
-        numberPhoneMessage.style.display = "block";
-      }}
     });
   </script>
   <!--End check phone-->
@@ -241,7 +218,7 @@
     var errorMessagepass = document.getElementById("pass-message");
 
     // Gắn sự kiện input cho phần tử input
-    input.addEventListener("input", function () {
+    input.addEventListener("input", function() {
       // Kiểm tra độ mạnh yếu của mật khẩu
       var password = input.value;
       var strength = 0;
@@ -255,11 +232,10 @@
       strength += regex.test(password) ? 1 : 0;
 
       // Hiển thị thông báo tương ứng
-      if(password.length == 0){
+      if (password.length == 0) {
         errorMessagepass.innerText = ' ';
         errorMessagepass.style.display = "block";
-      }
-      else if (password.length < 8 || strength < 2) {
+      } else if (password.length < 8 || strength < 2) {
         errorMessagepass.innerText = "Mật khẩu yếu";
         errorMessagepass.style.display = "block";
         errorMessagepass.style.color = "#99CC00";
@@ -271,13 +247,11 @@
         errorMessagepass.innerText = "Mật khẩu mạnh";
         errorMessagepass.style.display = "block";
         errorMessagepass.style.color = "#00CC99";
-      }
-      else if (password.length < 21 || strength < 4) {
+      } else if (password.length < 21 || strength < 4) {
         errorMessagepass.innerText = "Mật khẩu rất mạnh";
         errorMessagepass.style.display = "block";
         errorMessagepass.style.color = "#00CC00";
-      }
-      else {
+      } else {
         errorMessagepass.style.display = "none";
       }
     });
@@ -289,7 +263,7 @@
     var passwordRepeat = document.getElementById("repeatPassword");
 
     // Gắn sự kiện input cho phần tử input
-    passwordRepeat.addEventListener("input", function () {
+    passwordRepeat.addEventListener("input", function() {
       var khongtrungkhop = document.getElementById("khongtrungkhop");
       var trungkhop = document.getElementById("trungkhop");
       var password = input.value;
@@ -301,13 +275,11 @@
       } else if (password2 == password) {
         trungkhop.style.display = "block";
         khongtrungkhop.style.display = "none";
-      }
-      else {
+      } else {
         trungkhop.style.display = "none";
         khongtrungkhop.style.display = "block";
       }
     });
-
   </script>
 
 
@@ -324,6 +296,7 @@
         confirmButtonText: 'Xác nhận'
       })
     }
+
     function kiemTra() {
 
       if (document.frmdangky.fullName.value.trim().length == 0 ||
@@ -338,23 +311,23 @@
         showAlert();
         return false;
       }
-      if(document.frmdangky.passWord.value != document.frmdangky.repeatPassword.value){
+      if (document.frmdangky.passWord.value != document.frmdangky.repeatPassword.value) {
         Swal.fire({
-        title: 'Thông báo!',
-        text: 'Mật khẩu và Nhập lại mật khẩu không trùng khớp!',
-        icon: 'warning',
-        confirmButtonText: 'Xác nhận'
-      })
-      return false;
+          title: 'Thông báo!',
+          text: 'Mật khẩu và Nhập lại mật khẩu không trùng khớp!',
+          icon: 'warning',
+          confirmButtonText: 'Xác nhận'
+        })
+        return false;
       }
-      if(document.frmdangky.passWord.value.length < 8){
+      if (document.frmdangky.passWord.value.length < 8) {
         Swal.fire({
-        title: 'Thông báo!',
-        text: 'Mật khẩu tối thiểu 8 ký tự!',
-        icon: 'warning',
-        confirmButtonText: 'Xác nhận'
-      })
-      return false;
+          title: 'Thông báo!',
+          text: 'Mật khẩu tối thiểu 8 ký tự!',
+          icon: 'warning',
+          confirmButtonText: 'Xác nhận'
+        })
+        return false;
       }
     }
   </script>
@@ -365,12 +338,12 @@
     var btn_eye = document.querySelector('#btn-eyes');
     var passwordtxt = document.frm.passWord;
     var flag_eye = false;
+
     function showHidePass() {
       if (flag_eye == false) {
         passwordtxt.type = 'text';
         flag_eye = true;
-      }
-      else {
+      } else {
         passwordtxt.type = 'password';
         flag_eye = false;
       }
@@ -424,7 +397,6 @@
     $("#ward").change(() => {
 
     })
-
   </script>
   <!--END: Script api tỉnh quận huyện xã-->
 
