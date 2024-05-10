0fcd1d54d87d488f32ded9a481e4bafd47375ae6
Quốc Bảo <113650395+baongne@users.noreply.github.com>
2024-05-10 15:52:50 +0700

    Update vutrudongho.sql

--- Database/vutrudongho.sql
+++ Database/vutrudongho.sql
@@ -111,7 +111,7 @@ CREATE TABLE `order` (
   `OrderTotal` double NOT NULL,
   `Address` varchar(150) NOT NULL,
   `PaymentID` varchar(4) NOT NULL,
-  `VoucherID` varchar(5) NOT NULL,
+  `VoucherID` varchar(5) NULL,
   `OrderStatus` varchar(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
